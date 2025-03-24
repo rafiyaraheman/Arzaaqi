367b9e929be22758e65ed5484d8c1a0856adace5
Rafiya Raheman <96605855+rafiyaraheman@users.noreply.github.com>
2025-03-24 22:40:37 +0530

    Update index.js

--- client/src/store/shop/cart-slice/index.js
+++ client/src/store/shop/cart-slice/index.js
@@ -1,5 +1,126 @@
+// import axios from "axios";
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   cartItems: [],
+//   isLoading: false,
+// };
+
+// export const addToCart = createAsyncThunk(
+//   "cart/addToCart",
+//   async ({ userId, productId, quantity }) => {
+//     const response = await axios.post(
+//       "http://localhost:5000/api/shop/cart/add",
+//       {
+//         userId,
+//         productId,
+//         quantity,
+//       }
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const fetchCartItems = createAsyncThunk(
+//   "cart/fetchCartItems",
+//   async (userId) => {
+//     const response = await axios.get(
+//       `http://localhost:5000/api/shop/cart/get/${userId}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const deleteCartItem = createAsyncThunk(
+//   "cart/deleteCartItem",
+//   async ({ userId, productId }) => {
+//     const response = await axios.delete(
+//       `http://localhost:5000/api/shop/cart/${userId}/${productId}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const updateCartQuantity = createAsyncThunk(
+//   "cart/updateCartQuantity",
+//   async ({ userId, productId, quantity }) => {
+//     const response = await axios.put(
+//       "http://localhost:5000/api/shop/cart/update-cart",
+//       {
+//         userId,
+//         productId,
+//         quantity,
+//       }
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// const shoppingCartSlice = createSlice({
+//   name: "shoppingCart",
+//   initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(addToCart.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(addToCart.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.cartItems = action.payload.data;
+//       })
+//       .addCase(addToCart.rejected, (state) => {
+//         state.isLoading = false;
+//         state.cartItems = [];
+//       })
+//       .addCase(fetchCartItems.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(fetchCartItems.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.cartItems = action.payload.data;
+//       })
+//       .addCase(fetchCartItems.rejected, (state) => {
+//         state.isLoading = false;
+//         state.cartItems = [];
+//       })
+//       .addCase(updateCartQuantity.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(updateCartQuantity.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.cartItems = action.payload.data;
+//       })
+//       .addCase(updateCartQuantity.rejected, (state) => {
+//         state.isLoading = false;
+//         state.cartItems = [];
+//       })
+//       .addCase(deleteCartItem.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(deleteCartItem.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.cartItems = action.payload.data;
+//       })
+//       .addCase(deleteCartItem.rejected, (state) => {
+//         state.isLoading = false;
+//         state.cartItems = [];
+//       });
+//   },
+// });
+
+// export default shoppingCartSlice.reducer;
+
+
+
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const initialState = {
   cartItems: [],
@@ -9,14 +130,11 @@ const initialState = {
 export const addToCart = createAsyncThunk(
   "cart/addToCart",
   async ({ userId, productId, quantity }) => {
-    const response = await axios.post(
-      "http://localhost:5000/api/shop/cart/add",
-      {
-        userId,
-        productId,
-        quantity,
-      }
-    );
+    const response = await axios.post(`${API_URL}/api/shop/cart/add`, {
+      userId,
+      productId,
+      quantity,
+    });
 
     return response.data;
   }
@@ -25,9 +143,7 @@ export const addToCart = createAsyncThunk(
 export const fetchCartItems = createAsyncThunk(
   "cart/fetchCartItems",
   async (userId) => {
-    const response = await axios.get(
-      `http://localhost:5000/api/shop/cart/get/${userId}`
-    );
+    const response = await axios.get(`${API_URL}/api/shop/cart/get/${userId}`);
 
     return response.data;
   }
@@ -37,7 +153,7 @@ export const deleteCartItem = createAsyncThunk(
   "cart/deleteCartItem",
   async ({ userId, productId }) => {
     const response = await axios.delete(
-      `http://localhost:5000/api/shop/cart/${userId}/${productId}`
+      `${API_URL}/api/shop/cart/${userId}/${productId}`
     );
 
     return response.data;
@@ -47,14 +163,11 @@ export const deleteCartItem = createAsyncThunk(
 export const updateCartQuantity = createAsyncThunk(
   "cart/updateCartQuantity",
   async ({ userId, productId, quantity }) => {
-    const response = await axios.put(
-      "http://localhost:5000/api/shop/cart/update-cart",
-      {
-        userId,
-        productId,
-        quantity,
-      }
-    );
+    const response = await axios.put(`${API_URL}/api/shop/cart/update-cart`, {
+      userId,
+      productId,
+      quantity,
+    });
 
     return response.data;
   }
