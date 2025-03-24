15abad91c676b1ff9e5ba63821113d392a991296
Rafiya Raheman <96605855+rafiyaraheman@users.noreply.github.com>
2025-03-24 22:42:50 +0530

    Update index.js

--- client/src/store/shop/review-slice/index.js
+++ client/src/store/shop/review-slice/index.js
@@ -1,5 +1,58 @@
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   isLoading: false,
+//   reviews: [],
+// };
+
+// export const addReview = createAsyncThunk(
+//   "/order/addReview",
+//   async (formdata) => {
+//     const response = await axios.post(
+//       `http://localhost:5000/api/shop/review/add`,
+//       formdata
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// export const getReviews = createAsyncThunk("/order/getReviews", async (id) => {
+//   const response = await axios.get(
+//     `http://localhost:5000/api/shop/review/${id}`
+//   );
+
+//   return response.data;
+// });
+
+// const reviewSlice = createSlice({
+//   name: "reviewSlice",
+//   initialState,
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(getReviews.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(getReviews.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.reviews = action.payload.data;
+//       })
+//       .addCase(getReviews.rejected, (state) => {
+//         state.isLoading = false;
+//         state.reviews = [];
+//       });
+//   },
+// });
+
+// export default reviewSlice.reducer;
+
+
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const initialState = {
   isLoading: false,
@@ -9,20 +62,13 @@ const initialState = {
 export const addReview = createAsyncThunk(
   "/order/addReview",
   async (formdata) => {
-    const response = await axios.post(
-      `http://localhost:5000/api/shop/review/add`,
-      formdata
-    );
-
+    const response = await axios.post(`${API_URL}/api/shop/review/add`, formdata);
     return response.data;
   }
 );
 
 export const getReviews = createAsyncThunk("/order/getReviews", async (id) => {
-  const response = await axios.get(
-    `http://localhost:5000/api/shop/review/${id}`
-  );
-
+  const response = await axios.get(`${API_URL}/api/shop/review/${id}`);
   return response.data;
 });
 
