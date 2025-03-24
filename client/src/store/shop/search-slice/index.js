da0b429bbe5a9cad68e398f0a9ccca9f48117982
Rafiya Raheman <96605855+rafiyaraheman@users.noreply.github.com>
2025-03-24 22:43:34 +0530

    Update index.js

--- client/src/store/shop/search-slice/index.js
+++ client/src/store/shop/search-slice/index.js
@@ -1,5 +1,55 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// const initialState = {
+//   isLoading: false,
+//   searchResults: [],
+// };
+
+// export const getSearchResults = createAsyncThunk(
+//   "/order/getSearchResults",
+//   async (keyword) => {
+//     const response = await axios.get(
+//       `http://localhost:5000/api/shop/search/${keyword}`
+//     );
+
+//     return response.data;
+//   }
+// );
+
+// const searchSlice = createSlice({
+//   name: "searchSlice",
+//   initialState,
+//   reducers: {
+//     resetSearchResults: (state) => {
+//       state.searchResults = [];
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(getSearchResults.pending, (state) => {
+//         state.isLoading = true;
+//       })
+//       .addCase(getSearchResults.fulfilled, (state, action) => {
+//         state.isLoading = false;
+//         state.searchResults = action.payload.data;
+//       })
+//       .addCase(getSearchResults.rejected, (state) => {
+//         state.isLoading = false;
+//         state.searchResults = [];
+//       });
+//   },
+// });
+
+// export const { resetSearchResults } = searchSlice.actions;
+
+// export default searchSlice.reducer;
+
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 const initialState = {
   isLoading: false,
@@ -9,10 +59,7 @@ const initialState = {
 export const getSearchResults = createAsyncThunk(
   "/order/getSearchResults",
   async (keyword) => {
-    const response = await axios.get(
-      `http://localhost:5000/api/shop/search/${keyword}`
-    );
-
+    const response = await axios.get(`${API_URL}/api/shop/search/${keyword}`);
     return response.data;
   }
 );
