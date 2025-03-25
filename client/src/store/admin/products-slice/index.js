53df82033613413a29ba36fdd0de22b93c902bfa
Rafiya Raheman <96605855+rafiyaraheman@users.noreply.github.com>
2025-03-25 11:55:50 +0530

    Update index.js

--- client/src/store/admin/products-slice/index.js
+++ client/src/store/admin/products-slice/index.js
@@ -87,52 +87,68 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 const initialState = {
   isLoading: false,
   productList: [],
 };
 
 export const addNewProduct = createAsyncThunk(
-  "/products/addNewProduct",
+  "/products/addnewproduct",
   async (formData) => {
-    const result = await axios.post(`${API_URL}/api/admin/products/add`, formData, {
-      headers: { "Content-Type": "application/json" },
-    });
+    const result = await axios.post(
+      `${import.meta.env.VITE_API_URL}/api/admin/products/add`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    return result.data;
+    return result?.data;
   }
 );
 
 export const fetchAllProducts = createAsyncThunk(
   "/products/fetchAllProducts",
   async () => {
-    const result = await axios.get(`${API_URL}/api/admin/products/get`);
-    return result.data;
+    const result = await axios.get(
+      `${import.meta.env.VITE_API_URL}/api/admin/products/get`
+    );
+
+    return result?.data;
   }
 );
 
 export const editProduct = createAsyncThunk(
   "/products/editProduct",
   async ({ id, formData }) => {
-    const result = await axios.put(`${API_URL}/api/admin/products/edit/${id}`, formData, {
-      headers: { "Content-Type": "application/json" },
-    });
+    const result = await axios.put(
+      `${import.meta.env.VITE_API_URL}/api/admin/products/edit/${id}`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    return result.data;
+    return result?.data;
   }
 );
 
 export const deleteProduct = createAsyncThunk(
   "/products/deleteProduct",
   async (id) => {
-    const result = await axios.delete(`${API_URL}/api/admin/products/delete/${id}`);
-    return result.data;
+    const result = await axios.delete(
+      `${import.meta.env.VITE_API_URL}/api/admin/products/delete/${id}`
+    );
+
+    return result?.data;
   }
 );
 
-const adminProductsSlice = createSlice({
+const AdminProductsSlice = createSlice({
   name: "adminProducts",
   initialState,
   reducers: {},
@@ -145,13 +161,11 @@ const adminProductsSlice = createSlice({
         state.isLoading = false;
         state.productList = action.payload.data;
       })
-      .addCase(fetchAllProducts.rejected, (state) => {
+      .addCase(fetchAllProducts.rejected, (state, action) => {
         state.isLoading = false;
         state.productList = [];
       });
   },
 });
 
-export default adminProductsSlice.reducer;
-
-
+export default AdminProductsSlice.reducer;
