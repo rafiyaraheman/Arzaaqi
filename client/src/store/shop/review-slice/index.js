9238a0af3f4fbced22910befcabbb759d7300609
Rafiya Raheman <96605855+rafiyaraheman@users.noreply.github.com>
2025-03-25 11:59:51 +0530

    Update index.js

--- client/src/store/shop/review-slice/index.js
+++ client/src/store/shop/review-slice/index.js
@@ -52,8 +52,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 const initialState = {
   isLoading: false,
   reviews: [],
@@ -62,13 +60,20 @@ const initialState = {
 export const addReview = createAsyncThunk(
   "/order/addReview",
   async (formdata) => {
-    const response = await axios.post(`${API_URL}/api/shop/review/add`, formdata);
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/api/shop/review/add`,
+      formdata
+    );
+
     return response.data;
   }
 );
 
 export const getReviews = createAsyncThunk("/order/getReviews", async (id) => {
-  const response = await axios.get(`${API_URL}/api/shop/review/${id}`);
+  const response = await axios.get(
+    `${import.meta.env.VITE_API_URL}/api/shop/review/${id}`
+  );
+
   return response.data;
 });
 
